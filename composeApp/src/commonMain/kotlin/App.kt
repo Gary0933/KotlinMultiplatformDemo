b0ebea1220b7f75d81030b2316258309e9cb3ef1
Gary0933 <gary_intel@163.com>
2024-05-11 16:11:02 +0800

    update 2024-05-11 16:04

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,11 @@
+import androidx.compose.runtime.*
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import ui.theme.DemoTheme
+
+@Composable
+@Preview
+fun App() {
+    DemoTheme{
+
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/ui/theme/Theme.kt
@@ -0,0 +1,32 @@
+package ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val DarkColors = darkColorScheme(
+    primary = md_theme_dark_primary,
+)
+
+private val LightColors = lightColorScheme(
+    primary = md_theme_light_primary,
+)
+
+@Composable
+fun DemoTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> DarkColors
+        else -> LightColors
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
