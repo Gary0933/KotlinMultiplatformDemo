7a34289c05df76af4daf2b5b816d116d2bb7a297
Gary0933 <gary_intel@163.com>
2024-06-14 09:13:09 +0800

    update

--- composeApp/src/commonMain/kotlin/ui/components/Buttons.kt
+++ composeApp/src/commonMain/kotlin/ui/components/Buttons.kt
@@ -7,24 +7,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import kotlinmultiplatformdemo.composeapp.generated.resources.Res
-import kotlinmultiplatformdemo.composeapp.generated.resources.sign_up
-import org.jetbrains.compose.resources.stringResource
-import org.jetbrains.skia.PathEffect
 import ui.theme.BorderColor
 
 @Composable
